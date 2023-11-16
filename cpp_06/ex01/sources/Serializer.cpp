1d75333096016714d27cd20037668241fa5d6f9c
Thea Wang <twang@z4r7p3.42lyon.fr>
2023-11-16 17:29:12 +0100

    add: cpp06 : ex01 done ?

--- cpp_06/ex01/sources/Serializer.cpp
+++ cpp_06/ex01/sources/Serializer.cpp
@@ -6,7 +6,7 @@
 /*   By: twang <twang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 15:50:29 by twang             #+#    #+#             */
-/*   Updated: 2023/11/16 17:19:37 by twang            ###   ########.fr       */
+/*   Updated: 2023/11/16 17:25:20 by twang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,14 +42,14 @@ Serializer & Serializer::operator=( Serializer const & right_value )
 
 uintptr_t	Serializer::serialize( Data *ptr )
 {
-	uintptr_t	u_i = reinterpret_cast<uintptr_t>( ptr );
+	uintptr_t	u_i = reinterpret_cast< uintptr_t >( ptr );
 
 	return ( u_i );
 }
 
 Data*		Serializer::deserialize( uintptr_t raw )
 {
-	Data	*Data = reinterpret_cast<Data *>( raw );
+	Data	*Data = reinterpret_cast< ::Data * >( raw );
 
 	return ( Data );
 }
--- cpp_06/ex01/sources/main.cpp
+++ cpp_06/ex01/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: twang <twang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 13:30:20 by twang             #+#    #+#             */
-/*   Updated: 2023/11/16 17:17:17 by twang            ###   ########.fr       */
+/*   Updated: 2023/11/16 17:28:09 by twang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,20 @@
 
 int	main( void )
 {
-	Data	*Data = new Data;
-	uintptr_t	i;
+	Data	*Data = new ::Data;
 
-	Serializer::serialize( Data );
+	uintptr_t	i = 10;
+	Data		= Serializer::deserialize( i );
+	
+	i = Serializer::serialize( Data );
+
+	std::cout << i << std::endl;
+
+	Data		= Serializer::deserialize( i );
+
+	i = Serializer::serialize( Data );
+
+	std::cout << i << std::endl;
 
 	return ( 0 );
 }
